89a6a80f09accef6b9ef85b98fa271b829fa5317
jordan62300 <jordanfievetpro@outlook.fr>
2019-06-21 00:58:27 +0200

    ajout du systeme de selection aléatoire de l'ia et de la detection d'un gagnat

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
-const choices = document.querySelectorAll('.choice')
+const choices = document.querySelectorAll('.choice');
 
 function play(e) {
-    const mainJoueur = e.target.id ;
-    console.log(mainJoueur)
-    console.log('hello deux')
+    const playerHand = e.target.id ;
+    console.log(playerHand);
+   iaHand = getRandomHand();
+   console.log('choix de IA ' + iaHand)
+   const winner = getWinner(playerHand,iaHand);
+   console.log(`${winner}`);
+
 }
 
-console.log('hello')
+function getRandomHand() {
+  let  randomNumberBetween1To3 =   Math.floor(Math.random() * (4 - 1)) + 1;
+    if(randomNumberBetween1To3 == 1) {
+        return 'pierre';
+    } else if(randomNumberBetween1To3 == 2) {
+        return 'feuille'
+    } else {
+        return 'ciseau'
+    }
+   
+}
+
+function getWinner(playerHand,iaHand) {
+    if(playerHand == iaHand ) {
+        return 'its a draw';
+    } 
+    else if(playerHand == 'ciseau') {
+        if(iaHand == 'feuille') {
+            return 'the player win'
+        } else {
+            return 'the ia win'
+        }
+    }
+    else if(playerHand == 'feuille') {
+        if(iaHand == 'pierre') {
+            return 'the player win'
+        } else {
+            return 'the ia win'
+        }
+    }
+    else if(playerHand == 'pierre') {
+        if(iaHand == 'ciseau') {
+            return 'the player win'
+        } else {
+            return 'the ia win'
+        }
+    
+}
+}
+
+
+
 
 // Clear modal
 
